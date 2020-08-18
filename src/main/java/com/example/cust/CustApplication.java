34712a430cbcaeaffa3583fe493183c35542927b
v-xiongcai <1955383721@qq.com>
2020-08-18 16:09:00 +0800

    task#0 新建项目

---
+++ src/main/java/com/example/cust/CustApplication.java
@@ -0,0 +1,13 @@
+package com.example.cust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cust/CustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
